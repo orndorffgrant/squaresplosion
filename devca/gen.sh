0552d61b59c8b95a7e4b83e230e5f628e0f45eea
Grant Orndorff <orndorffgrant@gmail.com>
2020-06-14 19:40:46 -0400

    still not working but saving progress

--- devca/gen.sh
+++ devca/gen.sh
@@ -1,12 +1,16 @@
 set -x
 set -e
-openssl genrsa -out devca.key 2048
-openssl req -x509 -new -nodes -key devca.key -sha256 -days 1825 -out devca.pem -subj '/CN=devca'
-openssl genrsa -out localhost.key 2048
-openssl req -new -key localhost.key -out localhost.csr -subj '/CN=localhost'
-openssl x509 -req -in localhost.csr -CA devca.pem -CAkey devca.key -CAcreateserial \
-    -out localhost.crt -days 365 -sha256 -extfile <( \
-    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+# might need -keyout devcakey.pem
+openssl req -x509 -config openssl-ca.cnf -newkey rsa:4096 -sha256 -nodes -out devcacert.pem -outform PEM
+
+
+# openssl genrsa -out devca.key 2048
+# openssl req -x509 -new -nodes -key devca.key -sha256 -days 1825 -out devca.pem -subj '/CN=devca'
+# openssl genrsa -out localhost.key 2048
+# openssl req -new -key localhost.key -out localhost.csr -subj '/CN=localhost'
+# openssl x509 -req -in localhost.csr -CA devca.pem -CAkey devca.key -CAcreateserial \
+#     -out localhost.crt -days 365 -sha256 -extfile <( \
+#     printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
 
 # openssl req -x509 -out localhost.crt -keyout localhost.key \
@@ -16,5 +20,4 @@ openssl x509 -req -in localhost.csr -CA devca.pem -CAkey devca.key -CAcreateseri
 
 mkdir -p ~/.mozilla/certificates
 cp devca.pem ~/.mozilla/certificates
-mkdir -p /etc/firefox/distribution
-cp policies.json /etc/firefox/distribution
+cp policies.json /usr/lib/firefox/distribution/
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 trap 'kill $(jobs -p)' EXIT
-npx serve --ssl-cert minica/localhost.crt --ssl-key minica/localhost.key frontend &
+npx serve --ssl-cert devca/localhost.crt --ssl-key devca/localhost.key frontend &
 cd backend
-cargo run localhost:9999 --cert ../minica/localhost.crt --key ../minica/localhost.key
+cargo run localhost:9999 --cert ../devca/localhost.crt --key ../devca/localhost.key
