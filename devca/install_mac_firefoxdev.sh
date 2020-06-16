b28ead16b4625f4f91ee00f26d78010f1f11120d
Grant Orndorff <orndorffgrant@gmail.com>
2020-06-16 09:36:22 -0400

    fix mac install script

--- devca/install_mac_firefoxdev.sh
+++ devca/install_mac_firefoxdev.sh
@@ -1,6 +1,7 @@
 set -x
 set -e
 
-mkdir -p ~/Library/Application Support/Mozilla/Certificates
-cp devcacert.pem ~/Library/Application Support/Mozilla/Certificates
-cp policies.json "Firefox Developer Edition.app/Contents/Resources/distribution"
+mkdir -p "/Library/Application Support/Mozilla/Certificates"
+cp devcacert.pem "/Library/Application Support/Mozilla/Certificates"
+mkdir -p "/Applications/Firefox Developer Edition.app/Contents/Resources/distribution"
+cp policies.json "/Applications/Firefox Developer Edition.app/Contents/Resources/distribution"
